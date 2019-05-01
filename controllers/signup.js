bdb2b2094d84171d90d0cad8595a75d2974ef3ee
pawc <pswid89@gmail.com>
2019-05-01 12:29:26 +0200

    sign up post request validation

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,4 +1,5 @@
 var seq = require('../sequelize.js');
+var createError = require('http-errors');
 
 const signupGet = ((req, res, next) => {
     res.render('signup');
@@ -7,8 +8,18 @@ const signupGet = ((req, res, next) => {
 const signupPost = ((req, res, next) => {
     var login = req.body.login;
     var password = req.body.password;
-    seq.register(login, password);
-    res.render('signin');
+
+    if(login.length <= 3 || password.length <= 3){
+        res.locals.message = 'Login and password need to be over 3 characters long.';
+        res.locals.error = createError(406);
+        res.status(res.locals.error.status || 406);
+        res.render('error');
+    }
+    else{      
+        seq.register(login, password);
+        res.render('signin'); 
+    }
+
 });
 
 module.exports = {
