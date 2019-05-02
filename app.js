53aad2ffe4452696ac2e2bd4f7894a846ae8d84e
pawc <pswid89@gmail.com>
2019-05-02 14:04:55 +0200

    references to static files

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/node_modules/'));
+app.use('/public', express.static(__dirname + '/public/'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 var crypto = require('./utils/crypto.js');
 app.use(session({
