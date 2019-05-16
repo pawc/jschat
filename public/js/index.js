52e2d9563569400e58e87a4b5a6eeddcb43ba8ee
pawc <pswid89@gmail.com>
2019-05-16 22:12:12 +0200

    navbar change

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,38 @@ $(document).ready(() => {
     $("#usersLink").click(function(event){
         event.preventDefault(); 
         $("#content").attr("src", "/users");
+        setActive('users');
     });
 
     $("#profileLink").click(function(event){
         event.preventDefault(); 
         $("#content").attr("src", "/profile");
+        setActive('profile');
     });
 
     $("#chatLink").click(function(event){
         event.preventDefault(); 
         $("#content").attr("src", "/chat");
+        setActive('chat');
     });
 
 });
+
+function setActive(link){
+    $("#profileNav").removeClass("active");
+    $("#usersNav").removeClass("active");
+    $("#chatNav").removeClass("active");
+
+    if(link == 'users'){
+        $('#usersNav').addClass("active");
+    }
+
+    if(link == 'profile'){
+        $('#profileNav').addClass("active");
+    }
+
+    if(link == 'chat'){
+        $('#chatNav').addClass("active");
+    }
+
+}
